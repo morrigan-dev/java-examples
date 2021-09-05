7e51263fcd2449ca20e3726ff821c0e6da460228
Morrigan <morrigan.dev@gmail.com>
2021-09-05 12:42:11 +0200

    ergänzt Kommentare

--- java-examples/java-examples-mina-sshd/src/main/java/de/morrigan/dev/examples/mina/sshd/example1/Example1.java
+++ java-examples/java-examples-mina-sshd/src/main/java/de/morrigan/dev/examples/mina/sshd/example1/Example1.java
@@ -24,157 +24,155 @@ import de.morrigan.dev.examples.mina.sshd.SshConnection;
 import de.morrigan.dev.examples.mina.sshd.SshResponse;
 import de.morrigan.dev.examples.mina.sshd.SshTimeoutException;
 
+// TODO sshd mock und tests erstellen
 public class Example1 {
 
-	/** Logger for debugging */
-	private static final Logger LOG = LoggerFactory.getLogger(Example1.class);
+  /** Logger for debugging */
+  private static final Logger LOG = LoggerFactory.getLogger(Example1.class);
 
-	public static void main(String[] args) throws SshTimeoutException, IOException {
-		String username = args[0];
-		String password = args[1];
-		String host = args[2];
-		SshConnection conn = new SshConnection(username, password, host);
-		SshResponse response;
+  public static void main(String[] args) throws SshTimeoutException, IOException {
+    String username = args[0];
+    String password = args[1];
+    String host = args[2];
+    SshConnection conn = new SshConnection(username, password, host);
+    SshResponse response;
 
-		response = runSingleCommand(conn, "cd Bilder", 5);
-		LOG.info("returnCode: {}, errMsg: {}, stdOut: {}", response.getReturnCode(), response.getErrOutput(),
-				response.getStdOutput());
-		response = runSingleCommand(conn, "ls -la", 5);
-		LOG.info("returnCode: {}, errMsg: {}, stdOut: {}", response.getReturnCode(), response.getErrOutput(),
-				response.getStdOutput());
+    response = runSingleCommand(conn, "cd Bilder", 5);
+    LOG.info("returnCode: {}, errMsg: {}, stdOut: {}", response.getReturnCode(), response.getErrOutput(),
+        response.getStdOutput());
+    response = runSingleCommand(conn, "ls -la", 5);
+    LOG.info("returnCode: {}, errMsg: {}, stdOut: {}", response.getReturnCode(), response.getErrOutput(),
+        response.getStdOutput());
 
-		String[] commands = { 
-				"cd scripts", 
-				"ls -la",
-				"./setup.sh",
-				"echo \"$ROOT_DIR\""
-		};
-		response = runMultipleCommandsInSameSession(conn, commands, 10);
-		LOG.info("returnCode: {}, errMsg: {}, stdOut: {}", response.getReturnCode(), response.getErrOutput(),
-				response.getStdOutput());
-	}
+    String[] commands = {
+        "cd scripts",
+        "ls -la",
+        "./setup.sh",
+        "echo \"$ROOT_DIR\""
+    };
+    response = runMultipleCommandsInSameSession(conn, commands, 10);
+    LOG.info("returnCode: {}, errMsg: {}, stdOut: {}", response.getReturnCode(), response.getErrOutput(),
+        response.getStdOutput());
+  }
 
-	/**
-	 * Runs a SSH command against a remote system.
-	 * 
-	 * @param conn    Defines the connection to the system.
-	 * @param cmd     The command to run. Should generally be fully qualified (ex.
-	 *                instead of 'ls -la', use '/bin/ls -la')
-	 * @param timeout The amount of time to wait for the command to run before
-	 *                timing out. This is in seconds. This is used as two separate
-	 *                timeouts, one for login another for command execution.
-	 * @return The {@link SshResponse} contains the output of a successful command.
-	 * @throws SshTimeoutException Raised if the command times out.
-	 * @throws IOException         Raised in the event of a general failure (wrong
-	 *                             authentication or something of that nature).
-	 */
-	public static SshResponse runSingleCommand(SshConnection conn, String cmd, long timeout)
-			throws SshTimeoutException, IOException {
-		SshClient client = SshClient.setUpDefaultClient();
-		client.setServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE);
-		client.setHostConfigEntryResolver(HostConfigEntryResolver.EMPTY);
-		client.setKeyIdentityProvider(KeyIdentityProvider.EMPTY_KEYS_PROVIDER);
+  /**
+   * Runs a SSH command against a remote system.
+   * 
+   * @param conn Defines the connection to the system.
+   * @param cmd The command to run. Should generally be fully qualified (ex. instead of 'ls -la', use '/bin/ls -la')
+   * @param timeout The amount of time to wait for the command to run before timing out. This is in seconds. This is
+   *          used as two separate timeouts, one for login another for command execution.
+   * @return The {@link SshResponse} contains the output of a successful command.
+   * @throws SshTimeoutException Raised if the command times out.
+   * @throws IOException Raised in the event of a general failure (wrong authentication or something of that nature).
+   */
+  public static SshResponse runSingleCommand(SshConnection conn, String cmd, long timeout)
+      throws SshTimeoutException, IOException {
+    SshClient client = SshClient.setUpDefaultClient();
+    client.setServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE);
+    client.setHostConfigEntryResolver(HostConfigEntryResolver.EMPTY);
+    client.setKeyIdentityProvider(KeyIdentityProvider.EMPTY_KEYS_PROVIDER);
 
-		// Open the client
-		client.start();
+    // Open the client
+    client.start();
 
-		// Connect to the server
-		try (ClientSession session = createClientSession(client, conn, timeout);
-				ChannelExec ce = session.createExecChannel(cmd);
-				ByteArrayOutputStream out = new ByteArrayOutputStream();
-				ByteArrayOutputStream err = new ByteArrayOutputStream();) {
+    // Connect to the server
+    try (ClientSession session = createClientSession(client, conn, timeout);
+        ChannelExec ce = session.createExecChannel(cmd);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ByteArrayOutputStream err = new ByteArrayOutputStream();) {
 
-			ce.setOut(out);
-			ce.setErr(err);
+      ce.setOut(out);
+      ce.setErr(err);
 
-			// Execute and wait
-			ce.open();
-			Set<ClientChannelEvent> events = ce.waitFor(EnumSet.of(ClientChannelEvent.CLOSED),
-					TimeUnit.SECONDS.toMillis(timeout));
-			session.close(false);
+      // Execute and wait
+      ce.open();
+      Set<ClientChannelEvent> events = ce.waitFor(EnumSet.of(ClientChannelEvent.CLOSED),
+          TimeUnit.SECONDS.toMillis(timeout));
+      session.close(false);
 
-			// Check if timed out
-			if (events.contains(ClientChannelEvent.TIMEOUT)) {
-				throw new SshTimeoutException(cmd, conn.getHostname(), timeout);
-			}
+      // Check if timed out
+      if (events.contains(ClientChannelEvent.TIMEOUT)) {
+        throw new SshTimeoutException(cmd, conn.getHostname(), timeout);
+      }
 
-			// Respond
-			return new SshResponse(out.toString(), err.toString(), ce.getExitStatus());
-		} finally {
-			client.stop();
-		}
-	}
+      // Respond
+      return new SshResponse(out.toString(), err.toString(), ce.getExitStatus());
+    } finally {
+      client.stop();
+    }
+  }
 
-	public static SshResponse runMultipleCommandsInSameSession(SshConnection conn, String[] cmds, long timeout)
-			throws SshTimeoutException, IOException {
-		SshClient client = SshClient.setUpDefaultClient();
-		client.setServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE);
-		client.setHostConfigEntryResolver(HostConfigEntryResolver.EMPTY);
-		client.setKeyIdentityProvider(KeyIdentityProvider.EMPTY_KEYS_PROVIDER);
+  public static SshResponse runMultipleCommandsInSameSession(SshConnection conn, String[] cmds, long timeout)
+      throws SshTimeoutException, IOException {
+    SshClient client = SshClient.setUpDefaultClient();
+    client.setServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE);
+    client.setHostConfigEntryResolver(HostConfigEntryResolver.EMPTY);
+    client.setKeyIdentityProvider(KeyIdentityProvider.EMPTY_KEYS_PROVIDER);
 
-		// Open the client
-		client.start();
+    // Open the client
+    client.start();
 
-		try (ClientSession session = createClientSession(client, conn, timeout);
-				ClientChannel channel = session.createShellChannel();
-				PipedOutputStream pipedIn = new PipedOutputStream();
-				PipedInputStream pipedOut = new PipedInputStream(pipedIn)) {
+    try (ClientSession session = createClientSession(client, conn, timeout);
+        ClientChannel channel = session.createShellChannel();
+        PipedOutputStream pipedIn = new PipedOutputStream();
+        PipedInputStream pipedOut = new PipedInputStream(pipedIn)) {
 
-			channel.setIn(pipedOut);
+      channel.setIn(pipedOut);
 
-			try (ByteArrayOutputStream out = new ByteArrayOutputStream();
-					ByteArrayOutputStream err = new ByteArrayOutputStream()) {
+      try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+          ByteArrayOutputStream err = new ByteArrayOutputStream()) {
 
-				channel.setOut(out);
-				channel.setErr(err);
-				channel.open();
+        channel.setOut(out);
+        channel.setErr(err);
+        channel.open();
 
-				StringBuilder allCmds = new StringBuilder();
-				for (String cmd : cmds) {
-					allCmds.append(cmd).append(",");
-					if (!cmd.endsWith("\n")) {
-						cmd = cmd + "\n";
-					}
-					pipedIn.write(cmd.getBytes(StandardCharsets.UTF_8));
-					pipedIn.flush();
-				}
-				pipedIn.write("exit\n".getBytes(StandardCharsets.UTF_8));
-				pipedIn.flush();
+        StringBuilder allCmds = new StringBuilder();
+        for (String cmd : cmds) {
+          allCmds.append(cmd).append(",");
+          if (!cmd.endsWith("\n")) {
+            cmd = cmd + "\n";
+          }
+          pipedIn.write(cmd.getBytes(StandardCharsets.UTF_8));
+          pipedIn.flush();
+        }
+        pipedIn.write("exit\n".getBytes(StandardCharsets.UTF_8));
+        pipedIn.flush();
 
-				Set<ClientChannelEvent> events = channel.waitFor(EnumSet.of(ClientChannelEvent.CLOSED),
-						TimeUnit.SECONDS.toMillis(timeout));
+        Set<ClientChannelEvent> events = channel.waitFor(EnumSet.of(ClientChannelEvent.CLOSED),
+            TimeUnit.SECONDS.toMillis(timeout));
 
-				session.close(false);
-				client.stop();
+        session.close(false);
+        client.stop();
 
-				// Check if timed out
-				if (events.contains(ClientChannelEvent.TIMEOUT)) {
-					throw new SshTimeoutException(allCmds.toString(), conn.getHostname(), timeout);
-				}
+        // Check if timed out
+        if (events.contains(ClientChannelEvent.TIMEOUT)) {
+          throw new SshTimeoutException(allCmds.toString(), conn.getHostname(), timeout);
+        }
 
-				// Respond
-				return new SshResponse(out.toString(), err.toString(), channel.getExitStatus());
-			}
-		} finally {
-			client.stop();
-		}
-	}
+        // Respond
+        return new SshResponse(out.toString(), err.toString(), channel.getExitStatus());
+      }
+    } finally {
+      client.stop();
+    }
+  }
 
-	private static ClientSession createClientSession(SshClient client, SshConnection conn, long timeout)
-			throws IOException {
-		ClientSession session = client.connect(conn.getUsername(), conn.getHostname(), 22)
-				.verify(TimeUnit.SECONDS.toMillis(timeout)).getSession();
-		try {
-			session.addPasswordIdentity(conn.getPassword());
-			session.auth().verify(TimeUnit.SECONDS.toMillis(timeout));
+  private static ClientSession createClientSession(SshClient client, SshConnection conn, long timeout)
+      throws IOException {
+    ClientSession session = client.connect(conn.getUsername(), conn.getHostname(), 22)
+        .verify(TimeUnit.SECONDS.toMillis(timeout)).getSession();
+    try {
+      session.addPasswordIdentity(conn.getPassword());
+      session.auth().verify(TimeUnit.SECONDS.toMillis(timeout));
 
-			ClientSession returnValue = session;
-			session = null; // avoid 'finally' close
-			return returnValue;
-		} finally {
-			if (session != null) {
-				session.close();
-			}
-		}
-	}
+      ClientSession returnValue = session;
+      session = null; // avoid 'finally' close
+      return returnValue;
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
 }
